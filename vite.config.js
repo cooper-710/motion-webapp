a65158c568d6de36d73aa65b768b6946069f24b5
cooper-710 <cooprobinson7@gmail.com>
2025-08-19 15:51:29 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const base = process.env.BASE_PATH || "/";
+
+export default defineConfig({
+  base,
+  plugins: [react()],
+});
